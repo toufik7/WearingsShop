7cbf8d545acb534e6a07fa32a9406a10c5b691c9
Benmessabih Toufik <52804863+toufik7@users.noreply.github.com>
2021-04-15 17:14:16 +0100

    Add files via upload

--- shop/src/sample/GUI.java
+++ shop/src/sample/GUI.java
@@ -15,30 +15,33 @@ import static sample.methodes.table;
 public class GUI {
     private VBox layout,layout2;
     private Stage window; private Scene scene,scene2;
+    private String css;
     GUI(Stage primaryStage){
 
         Label label1 = makeLabel("Add new Products Here ! -------------------");
         Label label2 = makeLabel("Your Inventory Is Here ------------------");
+        css = this.getClass().getResource("index.css").toExternalForm();
+
 
         window = makeWindow(primaryStage);
 
         //creation peage
         Button inventory = makeInventoryBtn(window, scene2);
-        Button crt = makerCreatebtn(window,scene);
+        Button crt = makeCreatebtn(window,scene);
         Button help = makeHelpbtn();
         ToolBar toolbar = makeToolBar(crt,inventory,help);
         BorderPane borderPane = makeBorderPane(toolbar);
         layout = makeLayout(borderPane,label1);
-        scene = makeSeane(layout);
+        scene = makeSeane(layout,css);
 
         // inventory page
         Button inventory2 = makeInventoryBtn(window, scene2);
-        Button crt2 = makerCreatebtn(window,scene);
+        Button crt2 = makeCreatebtn(window,scene);
         Button help2 = makeHelpbtn();
-        ToolBar toolbar2 = makeToolBar(crt2,inventory2,help2);
+        ToolBar toolbar2 = makeToolBar(inventory2,crt2,help2);
         BorderPane borderPane2 = makeBorderPane(toolbar2);
         layout2 = makeLayout(borderPane2,label2,filters(),table());
-        scene2 = makeSeane(layout2);
+        scene2 = makeSeane(layout2,css);
 
         istart(window,scene2);
     }
